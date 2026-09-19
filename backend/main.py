288aaa50b2784ac92470a4a4b8817c379172d92f
Abir Sharma <abir.sharma21207@gmail.com>
2026-09-19 18:08:11 +0530

    FastAPI server setup

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,54 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Task(BaseModel):
+    title: str
+    completed: bool = False 
+
+tasks = [
+
+]
 
 @app.get("/")
 def home():
     return {"message": "Personal API is running!"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    return {"Error": "Task not found"}
+
+@app.post("/tasks")
+def create_task(task: Task):
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": task.title,
+        "completed": task.completed
+    }
+    tasks.append(new_task)
+    return new_task
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {"Message": "Task deleted"}
+    return {"Error": "Task not found"}
+
+@app.put("/tasks/{task_id}")
+def updated_task(task_id: int, updated_task: Task):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["completed"] = updated_task.completed
+            return task
+    return {"Error": "Task not found"}
